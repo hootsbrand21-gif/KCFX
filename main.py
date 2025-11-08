ae6a4d5f8b0bef01431552308938e20bdb785815
hootsbrand21-gif <hootsbrand21@gmail.com>
2025-11-08 14:09:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,8 @@ def logout():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+
 
 
